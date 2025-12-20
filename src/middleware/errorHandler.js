fc4e5be28d8c3590388831feb0ee6f1d2210cbf6
Kateryna Andriiak <151218983+mnkuffu@users.noreply.github.com>
2025-12-20 15:46:53 +0100

    Refactor error handling to use HttpError directly

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,7 @@
-import createHttpError from 'http-errors';
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (error, req, res, next) => {
-  if (error instanceof createHttpError.HttpError) {
+  if (error instanceof HttpError.HttpError) {
     return res.status(error.status).json({
       message: error.message,
     });

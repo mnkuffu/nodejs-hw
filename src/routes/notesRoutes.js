2d3959d9913eeab84b9dc8149a048f366ab03465
Kate <mnkuffu66@gmail.com>
2025-12-22 16:46:25 +0100

    hw3

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
 
 import {
   getAllNotes,
@@ -8,12 +9,19 @@ import {
   deleteNote,
 } from '../controllers/notesController.js';
 
+import {
+  getAllNotesSchema,
+  noteIdSchema,
+  createNoteSchema,
+  updateNoteSchema,
+} from '../validations/notesValidation.js';
+
 const router = Router();
 
-router.get('/', getAllNotes);
-router.get('/:noteId', getNoteById);
-router.post('/', createNote);
-router.patch('/:noteId', updateNote);
-router.delete('/:noteId',  deleteNote);
+router.get('/', celebrate(getAllNotesSchema), getAllNotes);
+router.get('/:noteId', celebrate(noteIdSchema), getNoteById);
+router.post('/', celebrate(createNoteSchema), createNote);
+router.patch('/:noteId', celebrate(updateNoteSchema), updateNote);
+router.delete('/:noteId', celebrate(noteIdSchema), deleteNote);
 
 export default router;
